b679c38f8b625bb51b56b613c140843d6974a3c7
Harsh Murari <hmurari@visionify.ai>
2023-02-07 18:51:45 -0700

    Migrate scenarios.json to this repo

--- visionai/config.py
+++ visionai/config.py
@@ -15,7 +15,6 @@ CONFIG_FOLDER = ROOT / 'config'
 CONFIG_FILE = ROOT / 'config' / 'config.json'
 SCENARIOS_SCHEMA = ROOT / 'config' / 'scenario-schema.json'
 SCENARIOS_URL = "https://github.com/visionify/visionai/blob/main/visionai/scenarios/scenarios.json"
-SCENARIOS_URL = "https://raw.githubusercontent.com/visionify/visionai-scenarios/main/scenarios.json" #TODO: Remove this once merged.
 
 # Triton server
 TRITON_HTTP_URL = 'http://localhost:8000'
